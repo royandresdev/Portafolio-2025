cc0797d8945ff819b881fc3a9f6b277570af0ba7
RoyAndresDev <royhuamanavila@gmail.com>
2025-05-10 22:20:31 -0500

    fix: corregir el nombre de la tecnología TailwindCSS en la sección de proyectos

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     image:
       "https://res.cloudinary.com/dcvwghrfp/image/upload/v1730772582/edutrack_banner.png",
     name: "EDUTRACK",
-    technologies: ["React", "TailwindCss", "Javascript"],
+    technologies: ["React", "TailwindCSS", "Javascript"],
     description:
       "Solución digital diseñada para mejorar la comunicación y el seguimiento del progreso académico entre estudiantes, padres y profesores. El objetivo es ofrecer un entorno donde el rendimiento académico pueda ser monitorizado de manera transparente y efectiva.",
     linkDemo: "",
