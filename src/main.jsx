91e02a3432fa3d80cf78af73bbed6287ff94c41a
RoyAndresDev <royhuamanavila@gmail.com>
2025-06-05 10:31:33 -0500

    feat: agregar soporte para Google Analytics y actualizar archivo de configuración

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const gtagId = import.meta.env.VITE_GTAG_ID;
+
+if (gtagId) {
+  const script = document.createElement("script");
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${gtagId}`;
+  script.async = true;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+
+  window.gtag = function () {
+    window.dataLayer.push(arguments);
+  };
+
+  window.gtag("js", new Date());
+  window.gtag("config", gtagId);
+} else {
+  console.error("Google Analytics ID no está definido.");
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
