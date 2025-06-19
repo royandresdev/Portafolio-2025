affc85a39b51f03e53fa4c70cf948baf968c21ac
royandresdev <royhuamanavila@gmail.com>
2025-06-19 09:23:29 -0500

    refactor: actualizar la carga del avatar y agregar archivo WEBP para mejorar rendimiento

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,5 +1,5 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
-import AvatarSrc from "../assets/Avatar.webp";
+import AvatarSrc from "/Avatar.webp";
 import { Frame } from "../components";
 
 const About = () => {
