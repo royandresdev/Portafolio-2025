d0291f96fb6beef6510eede1394eeaa5c6b7a1bb
royandresdev <royhuamanavila@gmail.com>
2025-06-20 18:57:52 -0500

    refactor: ajustar estilos en el componente BannerCookies para mejorar la responsividad

--- src/components/BannerCookies.jsx
+++ src/components/BannerCookies.jsx
@@ -3,13 +3,13 @@ import { Icon } from "@iconify/react/dist/iconify.js";
 function BannerCookies() {
   return (
     <div className="fixed bottom-0 left-0 right-0">
-      <div className="container flex gap-3 items-center bg-primary-400 text-primary-100 mx-auto mb-4 p-4 rounded-[20px]">
+      <div className="container flex flex-wrap gap-3 items-center bg-primary-400 text-primary-100 mx-auto mb-4 p-4 rounded-[20px]">
         <Icon
           icon="material-symbols:cookie"
           fontSize="2.5rem"
           color="var(--color-secondary-base)"
         />
-        <p className="text-primary-100">
+        <p className="text-primary-100 md:flex-1">
           Este sitio utiliza cookies para mejorar tu experiencia, analizar
           estadísticas y mostrar contenido embebido como videos de YouTube.
           Puedes aceptarlas o gestionarlas cuando lo desees.
