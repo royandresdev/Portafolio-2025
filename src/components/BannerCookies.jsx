19443b7dc10accaec889c6478544f85521d48df9
royandresdev <royhuamanavila@gmail.com>
2025-06-20 19:11:26 -0500

    feat: implementar gestión de consentimiento de cookies en el componente BannerCookies

--- src/components/BannerCookies.jsx
+++ src/components/BannerCookies.jsx
@@ -1,8 +1,29 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { useState } from "react";
 
 function BannerCookies() {
+  const [consented, setConsented] = useState(
+    localStorage.getItem("cookiesAccepted")
+  );
+  const [isOpen, setIsOpen] = useState(consented === null || consented === "false");
+
+  const handleAcceptCookies = () => {
+    localStorage.setItem("cookiesAccepted", "true");
+    setConsented(true);
+    setIsOpen(false);
+  };
+
+  const handleRejectCookies = () => {
+    localStorage.setItem("cookiesAccepted", "false");
+    setConsented(false);
+    setIsOpen(false);
+  }
+
   return (
-    <div className="fixed bottom-0 left-0 right-0">
+    <div
+      className={`fixed bottom-0 left-0 right-0 z-10 ${isOpen ? "fixed" : "hidden"}
+        }`}
+    >
       <div className="container flex flex-wrap gap-3 items-center bg-primary-400 text-primary-100 mx-auto mb-4 p-4 rounded-[20px]">
         <Icon
           icon="material-symbols:cookie"
@@ -15,8 +36,15 @@ function BannerCookies() {
           Puedes aceptarlas o gestionarlas cuando lo desees.
         </p>
         <div className="flex gap-4 ml-4">
-          <button className="text-primary-100 cursor-pointer hover:text-primary-200">Rechazar</button>
-          <button className="btn-secondary">Aceptar</button>
+          <button
+            className="text-primary-100 cursor-pointer hover:text-primary-200"
+            onClick={handleRejectCookies}
+          >
+            Rechazar
+          </button>
+          <button className="btn-secondary" onClick={handleAcceptCookies}>
+            Aceptar
+          </button>
         </div>
       </div>
     </div>
