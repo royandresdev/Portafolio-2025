0fb7157422f0b238bc984351cb0fb1f4a6f11a61
royandresdev <royhuamanavila@gmail.com>
2025-06-21 07:10:47 -0500

    feat: mejorar gestión de visualización de video según estado de consentimiento de cookies

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -13,7 +13,7 @@ const Experience = () => {
         <div className="flex flex-col lg:flex-row w-full gap-12 items-center flex-1 pb-12">
           <div className="w-full h-auto md:min-w-[650px] order-2 md:order-1">
             {
-              isAcceptedCookies && (
+              isAcceptedCookies ? (
                 <iframe
                   className="w-full aspect-video"
                   src="https://www.youtube-nocookie.com/embed/-SJfrF_vJ3A?si=TTO7f-YBR8tPcwUh"
@@ -22,6 +22,10 @@ const Experience = () => {
                   referrerPolicy="strict-origin-when-cross-origin"
                   allowFullScreen
                 ></iframe>
+              ) : (
+                <a href="https://www.youtube.com/watch?v=-SJfrF_vJ3A" target="_blank" rel="noopener noreferrer" title="TuVideo Project Video">
+                  <img src="https://res.cloudinary.com/dcvwghrfp/image/upload/v1750507576/TuVideo-YoutubePlaceholder.webp" alt="TuVideo Project Image" />
+                </a>
               )
             }
           </div>
